12286dccc43aa3ebfd0824a9d4b8a7b4c125d80c
plutoastronaut <145527980+plutoastronaut@users.noreply.github.com>
2024-03-12 23:00:27 +0800

    Add files via upload

---
+++ calculate+.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	a = 5684;
+	b = 4846;
+	c = a * b;
+	printf("c is %d\n", c);
+	return 0;
+}
---
+++ compare.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+	printf("求三个数中的最小值");
+		int a, b, c, min;
+		printf("请输入三个整数：");
+	scanf_s("%d,%d,%d",&a,&b,&c);
+	if (a < b) {
+		min = a;
+	}
+	else {
+		min = b;
+	}
+	if (c < min) {
+		min = c;
+	}
+	printf("%d\n",min);
+		return 0;
+}
