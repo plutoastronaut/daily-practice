d1863376e1390120aebd9245ee4d044b4a567527
plutoastronaut <145527980+plutoastronaut@users.noreply.github.com>
2024-04-04 13:04:25 +0800

    Add files via upload

---
+++ P99T4_6.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+int main()
+{
+	char s;
+	scanf_s("%c", &s);
+	printf("你的成绩为\n");
+
+	switch (s)
+	{
+	case'A':
+		printf("85⁓100\n");
+		break;
+	case'B':
+		printf("70⁓84\n");
+		break;
+	case'C':
+		printf("60⁓69\n");
+		break;
+	case'D':
+		printf("0⁓59\n");
+		break;
+	default:
+		printf("error");
+	}
+	return 0;
+}
